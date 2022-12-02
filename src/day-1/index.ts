3f3aec2b00e5dde17ed11201f0096e1369a90fa8
Clint Myers <clint.r.myers@gmail.com>
2022-12-01 20:39:35 -0600

    Day 1 - Final refactor

--- src/day-1/index.ts
+++ src/day-1/index.ts
@@ -1,38 +1,25 @@
 import fs from "fs/promises";
 import path from "path";
 
-/******************************/
-/* Utils                      */
-/******************************/
 const getData = async () => {
   return await fs.readFile(path.resolve(__dirname, "./input.txt"), {
     encoding: "utf8",
   });
 };
 
-/******************************/
-/* Part One - Start 1:20CST   */
-/******************************/
-export const partOne = async () => {
-  const data = await getData();
-  return Math.max(
-    ...data
-      .split("\n\n")
-      .map((x) => x.split("\n"))
-      .map((y) => y.map((z) => parseInt(z)).reduce((a, b) => a + b))
-  );
-};
-
-/*******************************/
-/* Part Two - Fin 1:46CST      */
-/*******************************/
-export const partTwo = async () => {
+const arrOfSums = (async () => {
   const data = await getData();
   return data
     .split("\n\n")
     .map((x) => x.split("\n"))
     .map((y) => y.map((z) => parseInt(z)).reduce((a, b) => a + b))
-    .sort()
-    .slice(-3)
-    .reduce((c, d) => c + d);
+    .sort();
+})();
+
+export const partOne = async () => {
+  return (await arrOfSums).slice(-1);
+};
+
+export const partTwo = async () => {
+  return (await arrOfSums).slice(-3).reduce((c, d) => c + d);
 };
